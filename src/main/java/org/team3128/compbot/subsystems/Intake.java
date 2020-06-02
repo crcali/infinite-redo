ca574a5336cd5346157a498fe101b86c47aa1e35
crcali <33082459+crcali@users.noreply.github.com>
2020-06-02 16:38:46 -0700

    Intake

--- src/main/java/org/team3128/compbot/subsystems/Intake.java
+++ src/main/java/org/team3128/compbot/subsystems/Intake.java
@@ -1,16 +1,21 @@
 package org.team3128.compbot.subsystems;
 
+import org.team3128.common.generics.Threaded;
+
 import org.team3128.common.hardware.motor.LazyCANSparkMax;
 import org.team3128.common.hardware.misc.Piston;
 import org.team3128.common.utility.Log;
 import edu.wpi.first.wpilibj.command.Command;
 
-public class Intake {
+public class Intake extends Threaded {
     public enum IntakeState
     {
-        INTAKE(true, "Intake"),
-        OUTTAKE(true, "Outtake"),
-        HOLDING(true, "Holding");
+        INTAKE_BOX(false, "Intake"),
+        OUTTAKE_BOX(false, "Outtake"),
+        HOLDING_BOX(false, "Holding"),
+        INTAKE_SMARTBELL(false, "Intake"),
+        OUTTAKE_SMARTBELL(false, "Outtake"),
+        HOLDING_SMARTBELLfalse, "Holding");
         private boolean PistonState;
         private String name;
         private IntakeState(boolean PistonState, String name) {
@@ -24,28 +29,32 @@ public class Intake {
             return name;
         }
     }
-    public LazyCANSparkMax intakeMotorTop, intakeMotorBottom;
+    public LazyCANSparkMax intakeMotorBoxTop, intakeMotorBoxBottom, intakeMotorSmartBellTop, intakeMotorSmartBellBottom;
     private IntakeState newState;
     public IntakeState currentState;
     private Piston Piston;
     private Thread cargoThread;
     private static Intake instance = null;
     public static Intake getInstance() {
+    
         if (instance != null) {
-            return instance;
-        }
-        Log.fatal("LiftIntake", "Attempted to get instance before initializtion! Call initialize(...) first.");
-        return null;
+        return instance;
     }
+    Log.fatal("LiftIntake", "Attempted to get instance before initializtion! Call initialize(...) first.");
+    return null;
+    }
+
     public static void initialize(LazyCANSparkMax intakeMotorTop, LazyCANSparkMax intakeMotorBottom, IntakeState state, Piston Piston) {
         instance = new Intake(intakeMotorTop, intakeMotorBottom, state, Piston);
     }
+
     private Intake(LazyCANSparkMax intakeMotorTop, LazyCANSparkMax intakeMotorBottom, IntakeState state, Piston Piston) {
         this.intakeMotorTop = intakeMotorTop;
         this.intakeMotorBottom = intakeMotorBottom;
         this.Piston = Piston;
-        setState(state);
+        this.newState = state;
     }
+
     public void setState(IntakeState newState) {
         if (this.currentState != newState) {
             if (newState.getPistonState()) {
@@ -57,25 +66,19 @@ public class Intake {
             this.newState = newState;
         }
     }
-    private void setIntakePower(double power) {
-        intakeMotorTop.set(power);
-        intakeMotorBottom.set(power);
+    private void setIntakePowerBox(double power) {
+        intakeMotorBoxTop.set(power);
+        intakeMotorBoxBottom.set(power);
     }
-    public class CmdSetLiftIntakeState extends Command {
-        IntakeState desiredState;
-        public CmdSetLiftIntakeState(IntakeState state) {
-            super(0.1);
-            desiredState = state;
-        }
-        @Override
-        protected void initialize()
-        {
-            setState(desiredState);
-        }
-        @Override
-        protected boolean isFinished()
-        {
-            return isTimedOut();
-        }
+    
+    private void setIntakePowerSmartBell(double power) {
+        intakeMotorSmartBellTop.set(power);
+        intakeMotorSmartBellBottom.set(power);
     }
+
+    @Override
+    public void update() {    
+        
+    }
+
 }
